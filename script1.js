42376559c6b9e388eff2460bcd41c896724dffd7
nextcorp <18381835+nextcorp@users.noreply.github.com>
2024-01-16 13:29:41 +0200

    date validation

--- script1.js
+++ script1.js
@@ -24,21 +24,53 @@ function reset(day, month, year) {
     hide(year)
 }
 
-function checkDate(day, month, year) {
+function displayMessage(where, message) {
+    const errOutput = document.querySelector(`#${where.id} ~ .err-message`)
+    errOutput.classList.remove("hidden")
+    errOutput.textContent = message
+    where.classList.add("invalid-input")
+}
 
+function checkDate(day, month, year) {
+    const today = new Date(Date.now())
+
+    if (year.value > today.getFullYear()) {
+        displayMessage(year, "Must be in the past")
+        return
+    }
+
+    if ((month.value < 1) || (month.value > 12)) {
+        displayMessage(month, "Must be a valid month")
+        return
+    }
+
+    if ((day.value < 1) || (day.value > 31)) {
+        displayMessage(day, "Must be a valid day")
+        return
+    }
+
+    const daysInMonth = new Date(today.getFullYear(), today.getMonth()-1, 0).getDate()
+
+    if (day.value > daysInMonth) {
+        reset(day, month, year)
+        displayMessage(day, "Must be a valid date")
+        month.classList.add("invalid-input")
+        year.classList.add("invalid-input")
+        return
+    }
+
+    if (new Date(year.value, month.value-1, day.value-1) > today) {
+        displayMessage(year, "Must be in the past")
+        return
+    }
 }
 
 function checkEmpty(day, month, year) {
-    const display = (where) => {
-        const errOutput = document.querySelector(`#${where.id} ~ .err-message`)
-        errOutput.classList.remove("hidden")
-        errOutput.textContent = "This field is required"
-        where.classList.add("invalid-input")
-    }
+    const message = "This field is required"
 
-    if (day.value.length === 0) display(day)
-    if (month.value.length === 0) display(month)
-    if (year.value.length === 0) display(year)
+    if (day.value.length === 0) displayMessage(day, message)
+    if (month.value.length === 0) displayMessage(month, message)
+    if (year.value.length === 0) displayMessage(year, message)
 }
 
 function submitButtonHandler(e) {
@@ -46,6 +78,7 @@ function submitButtonHandler(e) {
 
     reset(day, month, year)
     checkEmpty(day, month, year)
+    checkDate(day, month, year)
 }
 
 clear(day, month, year)
